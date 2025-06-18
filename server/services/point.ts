a3bdb07c42ade773a20a7f08e315b80baa6d49af
Victor Leonardi <victorleonardireis@poli.ufrj.br>
2025-06-17 23:03:47 -0300

    Fix minor issue

--- server/services/point.ts
+++ server/services/point.ts
@@ -1,6 +1,6 @@
 import { Point } from "../db/models/points";
 
 export async function getActivePoints() {
-  const points = await Point.find()
+  const points = await Point.find({ active: true })
   return points;
 }
