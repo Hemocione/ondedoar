c4a69dcc12328a2dbe491cf5317550d8f419f0ce
Victor Leonardi <victorleonardireis@poli.ufrj.br>
2025-06-25 21:47:06 -0300

    Fix minor issues

--- server/plugins/mongoose.ts
+++ server/plugins/mongoose.ts
@@ -4,9 +4,7 @@ export default defineNitroPlugin(async (_nitro) => {
 	if (mongoose.connection.readyState !== 1) {
 		const config = useRuntimeConfig();
 		try {
-			await mongoose.connect(config.db.mongo.uri, {
-				dbName: config.dbName,
-			});
+			await mongoose.connect(config.db.mongo.uri);
 		} catch (error: any) {
 			console.error("Failed to connect to MongoDB:", error);
 			throw error;
