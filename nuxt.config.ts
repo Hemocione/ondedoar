5f0f0a61debd6be3528260c3229cc831fc51ab8e
Victor Leonardi <victorleonardireis@poli.ufrj.br>
2025-06-25 21:46:17 -0300

    Fix uri and dbName

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,8 +19,7 @@ export default defineNuxtConfig({
   runtimeConfig: {
     db: {
       mongo: {
-        uri: process.env.MONGODB_URI ?? "mongodb://localhost:27017",
-        dbName: process.env.MONGODB_NAME ?? "local",
+        uri: process.env.MONGODB_URI ?? "mongodb://localhost:27017/local"
       }
     }
   }
