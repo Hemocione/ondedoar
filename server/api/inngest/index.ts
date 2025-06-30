628526c10d25790e9039236cbc88bf97a5d85dc2
Victor Leonardi <victorleonardireis@poli.ufrj.br>
2025-06-29 21:54:58 -0300

    Create eventHandler as demanded by doc

--- server/api/inngest/index.ts
+++ server/api/inngest/index.ts
@@ -1,5 +1,9 @@
 import { serve } from 'inngest/nuxt';
+import { taskManager } from '~/server/tasks';
 
 // Export Inngest here
 // TODO: Add the Inngest instance to the Nuxt context
-export default defineEventHandler(serve())
+export default defineEventHandler(serve({
+  client: taskManager.getInngest(),
+  functions: taskManager.jobs,
+}))
