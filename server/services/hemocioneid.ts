4cc79e20da30e3c0d45475087ba69433817fb081
Victor Leonardi <victorleonardireis@poli.ufrj.br>
2025-07-23 23:08:00 -0300

    Init HemocioneId service

---
+++ server/services/hemocioneid.ts
@@ -0,0 +1,46 @@
+const config = useRuntimeConfig()
+
+interface HemocioneIdPoint {
+  id: string;
+  name: string;
+  address: string;
+  latitude: number;
+  longitude: number;
+  displayName: string | null; // Pode ser string ou null
+}
+
+async function getHemocioneIdsPoints(after?: string): Promise<HemocioneIdPoint[] | undefined> {
+  try {
+    const hemocioneIdPoints: HemocioneIdPoint[] = await $fetch(`${config.hemocioneId.apiUrl}/points/onde-doar/sync`, {
+      method: 'GET',
+      headers: {
+        'x-secret': config.hemocioneId.backOfficeSecret
+      }
+    })
+    return hemocioneIdPoints
+  } catch (error) {
+    console.error(`Error fetching Hemocione ID points using after ${after}:`, error)
+    return
+  }
+}
+
+export async function handleHemocioneIdsPoints(after?: string) {
+  const hemocioneIdPoints = await getHemocioneIdsPoints(after)
+
+  if (!hemocioneIdPoints) {
+    throw new Error('Failed to fetch Hemocione ID points')
+  }
+
+  return hemocioneIdPoints.map((hemocioneIdPoint) => ({
+    name: hemocioneIdPoint.name,
+    address: hemocioneIdPoint.address,
+    phone: '', // TODO: Add phone if available in HemocioneId provider
+    link: '', // TODO: Add link if available in HemocioneId provider
+    active: true,
+    type: 'bloodbank',
+    loc: {
+      type: 'Point',
+      coordinates: [hemocioneIdPoint.longitude, hemocioneIdPoint.latitude]
+    }
+  }))
+}
