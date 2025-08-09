6e3d3119adb9cb6c3b64871a7e3db41c878ddb25
Victor Leonardi dos Reis <victorleonardireis@poli.ufrj.br>
2025-08-09 15:44:10 -0300

    Remove steps and improve job

--- server/tasks/jobs/syncHemocioneId.ts
+++ server/tasks/jobs/syncHemocioneId.ts
@@ -19,114 +19,96 @@ interface InactivatedPoints {
   modifiedIds: string[]
 }
 
-
-// TODO: REMOVE STEPS SINCE THEY COST MONEY. ENCAPSULATE IT WITH A TRY CATCH TO GRAB ERRORS
 export const syncHemocioneIdJob = async ({ event, step }: { event: any, step: Step }) => {
   console.log(`✅ Job '${event.name}' iniciado com sucesso!`);
-
   // Since will enable to run this job manually, we can use the event data to get the 'after' parameter
   // If 'after' is not provided, it will be set by last cursor and the job will fetch
   const after = event.data?.after || undefined;
 
-  const hemocioneIdPoints: HemocioneIdPointResponse[] = await step.run("get-handled-hemocione-id-points", async () => {
-    await SyncManager.updateOne(
-      { providerName: "HemocioneId" },
-      {
-        syncStatus: 'in_progress',
-      }
-    )
-    console.log("Getting and Handling Hemocione ID points...");
-    return await handleHemocioneIdsPoints(after);
-  })
+  await SyncManager.updateOne(
+    { providerName: "HemocioneId" },
+    {
+      syncStatus: 'in_progress',
+    }
+  )
+
+  console.log("Getting and Handling Hemocione ID points...");
+
+  const hemocioneIdPoints: HemocioneIdPointResponse[] = await handleHemocioneIdsPoints(after);
 
   if (!hemocioneIdPoints) {
     throw new Error('Failed to fetch Hemocione ID points');
   }
 
-  console.log("✅ Hemocione ID points handled successfully");
+  console.log("✅ Hemocione ID points handled successfully;", "\nUploading Hemocione ID points...");
 
-  const uploadedPoints: UploadedPoints = await step.run("upload-hemocione-id-points", async () => {
-    console.log("Uploading Hemocione ID points...");
-    // TODO: Think of a better key to filter and create a relation between points in hemocioneId and onde-doar
-    const operations = hemocioneIdPoints.map((hemocioneIdPoint) => {
-      return {
-        updateOne: {
-          filter: { name: hemocioneIdPoint.name },
-          update: {
-            $set: {
-              name: hemocioneIdPoint.name,
-              address: hemocioneIdPoint.address,
-              phone: hemocioneIdPoint.phone || '',
-              link: hemocioneIdPoint.link || '',
-              active: hemocioneIdPoint.active,
-              type: hemocioneIdPoint.type,
-              loc: {
-                type: 'Point',
-                coordinates: [hemocioneIdPoint.loc.coordinates[0], hemocioneIdPoint.loc.coordinates[1]]
-              }
-            },
-            $setOnInsert: {
-              createdAt: new Date(),
+  // TODO: Think of a better key to filter and create a relation between points in hemocioneId and onde-doar
+  const upsertOperations = hemocioneIdPoints.map((hemocioneIdPoint) => {
+    return {
+      updateOne: {
+        filter: { name: hemocioneIdPoint.name },
+        update: {
+          $set: {
+            name: hemocioneIdPoint.name,
+            address: hemocioneIdPoint.address,
+            phone: hemocioneIdPoint.phone || '',
+            link: hemocioneIdPoint.link || '',
+            active: hemocioneIdPoint.active,
+            type: hemocioneIdPoint.type,
+            loc: {
+              type: 'Point',
+              coordinates: [hemocioneIdPoint.loc.coordinates[0], hemocioneIdPoint.loc.coordinates[1]]
             }
           },
-          upsert: true, // Create if it doesn't exist
+          $setOnInsert: {
+            createdAt: new Date(),
+          }
+        },
+        upsert: true, // Create if it doesn't exist
+      }
+    }
+  }).filter(op => op !== null)
+
+  if (upsertOperations.length === 0) {
+    console.log("No points to upload");
+    return [];
+  }
+
+  const printThis = hemocioneIdPoints.map(p => p.name)
+
+  console.log(printThis)
+
+  const toInactivatePoints = await Point.find(
+    { active: true, name: { $nin: hemocioneIdPoints.map(p => p.name) } },
+    { _id: 1 }
+  )
+
+  const inactivateOperations = toInactivatePoints.map((point) => {
+    return {
+      updateOne: {
+        filter: { id: point.id },
+        update: {
+          $set: {
+            active: false
+          }
         }
       }
-    }).filter(op => op !== null)
-
-    if (operations.length === 0) {
-      console.log("No points to upload");
-      return [];
     }
+  });
 
-    const result = await Point.bulkWrite(operations)
+  const updatePoints = await Point.bulkWrite([...upsertOperations, ...inactivateOperations])
 
-    return {
-      matchedCount: result.matchedCount,
-      modifiedCount: result.modifiedCount,
-      upsertedCount: result.upsertedCount,
-      insertedIds: result.upsertedIds ? Object.values(result.upsertedIds) : []
+  await SyncManager.updateOne(
+    { providerName: "HemocioneId" },
+    {
+      lastSuccessfulSyncDate: new Date(),
+      lastSyncDate: new Date(),
+      syncStatus: 'completed',
+      syncErrors: null,
+      lastSyncResults: updatePoints.upsertedIds
     }
-  })
-
-  // TODO: Finish inactivating points that are not in the hemocioneIdPoints response
-  const inactivatedPoints: InactivatedPoints = await step.run("inactivate-hemocione-id-points", async () => {
-    console.log("Inactivating Hemocione ID points...");
-
-    const result = await Point.updateMany(
-      { active: true, name: { $nin: hemocioneIdPoints.map(p => p.name) } },
-      { $set: { active: false } }
-    );
-
-    const modifiedIds = await Point.find(
-      { active: false, name: { $nin: hemocioneIdPoints.map(p => p.name) } },
-      { _id: 1 }
-    ).then(points => points.map(point => point._id.toString()));
-
-    await SyncManager.updateOne(
-      { providerName: "HemocioneId" },
-      {
-        lastSuccessfulSyncDate: new Date(),
-        lastSyncDate: new Date(),
-        syncStatus: 'completed',
-        syncErrors: null
-      }
-    )
-
-    return {
-      matchedCount: result.matchedCount,
-      modifiedCount: result.modifiedCount,
-      modifiedIds: modifiedIds
-    }
-  })
+  )
 
   console.log("✅ Hemocione ID points uploaded and inactivated successfully")
   const message = `✅ Job '${event.name}' concluído com sucesso!`
-
-  // await step.run("finalizar-job", async () => {
-  //   // Aqui você poderia, por exemplo, salvar algo no banco de dados.
-  //   return { status: "concluído", message };
-  // });
-
-  // return { body: message };
 };
