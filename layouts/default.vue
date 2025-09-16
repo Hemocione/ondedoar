ebb5ce6061dfff1faf66fe69d3be1224655d578b
Victor Leonardi <victorleonardireis@poli.ufrj.br>
2025-09-16 00:22:23 -0300

    Define default layout

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="relative min-h-screen">
+    <div class="relative z-10">
+      <slot />
+    </div>
+
+    <MapLibreClient class="absolute inset-0" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import MapLibreClient from '~/components/MapLibre.client.vue';
+</script>
+
+<style></style>
