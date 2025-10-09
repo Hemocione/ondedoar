2fec4bfabba8f13d88cc39b29bb28274226cc05e
Victor Leonardi <victorleonardireis@poli.ufrj.br>
2025-10-09 20:08:49 -0300

    Adjust style

--- components/PlaceSearchInput.vue
+++ components/PlaceSearchInput.vue
@@ -2,10 +2,11 @@
   <div class="flex bg-transparent justify-center w-full">
     <UInput v-model="place" variant="soft" size="md" type="text" placeholder="Buscar endereço... "
       icon="i-heroicons-magnifying-glass"
-      :ui="{ base: 'bg-hemo-color-text-primary text-hemo-color-text-secondary rounded-full shadow-lg/5 w-[80vw] text-lg focus:bg-hemo-color-text-primary focus:text-hemo-color-text-secondary', leadingIcon: 'w-5 h-5', }">
+      :ui="{ base: 'bg-hemo-color-text-primary text-hemo-color-text-secondary rounded-full shadow-lg/5 w-[80vw] text-lg focus:bg-hemo-color-text-primary focus:text-hemo-color-text-secondary hover:bg-hemo-color-text-primary', leadingIcon: 'w-5 h-5', }">
       <template #trailing>
         <UButton variant="link" icon="i-heroicons-adjustments-horizontal"
-          :ui="{ base: 'text-hemo-color-text-secondary active:text-hemo-color-primary-light' }" @click="applyFilter" />
+          :ui="{ base: 'text-hemo-color-text-secondary active:text-hemo-color-primary-light hover:text-hemo-color-primary-action' }"
+          @click="applyFilter" />
       </template>
     </UInput>
   </div>
