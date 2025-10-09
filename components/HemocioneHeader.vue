bb57ed49dc593ff470f6c292c18981cae89fe089
Victor Leonardi <victorleonardireis@poli.ufrj.br>
2025-10-09 01:07:30 -0300

    Finish button style + Add todo's

--- components/HemocioneHeader.vue
+++ components/HemocioneHeader.vue
@@ -4,10 +4,38 @@
       <img class="w-[130px]" src="/assets/vectors/HemocioneWhite.svg"
         alt="Logo da Hemocione em branco e Hemocione escrito em branco">
     </div>
-    <UButton trailing-icon="i-heroicons-arrow-left-start-on-rectangle" size="md"
-      :ui="{ base: 'bg-hemo-color-primary text-hemo-color-text-primary' }">Sair
+    <UButton :trailing-icon="buttonIcon" size="md" :ui="{ base: 'bg-hemo-color-primary text-hemo-color-text-primary' }"
+      @click="buttonEvent">
+      {{ buttonText }}
     </UButton>
   </div>
 </template>
 
-<script lang="ts" setup></script>
+
+<!--  TODO: FINISH LOGIC FOR FUNCTIONS -->
+<!-- TODO: HANDLE LOGIN -->
+<script lang="ts" setup>
+const router = useRouter();
+
+const isLoggedIn = ref(false);
+
+const buttonText = computed(() => {
+  return isLoggedIn.value ? 'Sair' : 'Entrar';
+})
+
+const buttonIcon = computed(() => {
+  return isLoggedIn.value ? 'i-heroicons-arrow-left-on-rectangle' : 'i-heroicons-arrow-right-on-rectangle';
+})
+
+async function goRegister() {
+  console.log('goRegister');
+}
+
+async function logOut() {
+  console.log('logOut');
+}
+
+const buttonEvent = computed(() => {
+  return isLoggedIn.value ? logOut : goRegister
+})
+</script>
