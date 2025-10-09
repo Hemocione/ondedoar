e3270d7817fe87b17fe96ebfb2e783d5604b3f0b
Victor Leonardi <victorleonardireis@poli.ufrj.br>
2025-10-09 00:57:59 -0300

    Coded header component

--- components/HemocioneHeader.vue
+++ components/HemocioneHeader.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="container flex justify-center items-center w-screen h-[40px] bg-black">
-    <img src="/assets/vectors/HemocioneWhite.svg" alt="Logo da Hemocione em branco e Hemocione escrito em branco">
+  <div class="flex items-center w-screen h-[55px] bg-black px-4">
+    <div class="flex-grow">
+      <img class="w-[130px]" src="/assets/vectors/HemocioneWhite.svg"
+        alt="Logo da Hemocione em branco e Hemocione escrito em branco">
+    </div>
+    <UButton trailing-icon="i-heroicons-arrow-left-start-on-rectangle" size="md"
+      :ui="{ base: 'bg-hemo-color-primary text-hemo-color-text-primary' }">Sair
+    </UButton>
   </div>
 </template>
 
