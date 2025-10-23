3a71f1dcef9ff002fcd7b9565139375a2ad03dc3
Victor Leonardi dos Reis <victorleonardireis@poli.ufrj.br>
2025-10-23 16:41:03 -0300

    Make pins clickable

--- components/PinMarker.vue
+++ components/PinMarker.vue
@@ -1,6 +1,6 @@
 <template>
   <mgl-geo-json-source source-id="points" :data="geojsonSources">
-    <mgl-symbol-layer layer-id="points" :layout="layout" />
+    <mgl-symbol-layer layer-id="points" :layout="layout" @click="handleSymbolClick" />
   </mgl-geo-json-source>
 </template>
 
@@ -9,11 +9,12 @@ import {
   MglGeoJsonSource,
   MglSymbolLayer
 } from '@indoorequal/vue-maplibre-gl';
+import type { MapMouseEvent } from 'maplibre-gl';
 
 const props = defineProps<{
   features: {
     coordinates: number[],
-    symbol: string
+    [key: string]: any
   }[],
   zoom?: number
 }>()
@@ -21,15 +22,14 @@ const props = defineProps<{
 const geojsonSources = {
   type: 'FeatureCollection',
   features: props.features.map((feature) => {
+    const { coordinates, ...properties } = feature;
     return {
       type: 'Feature',
       geometry: {
         type: 'Point',
-        coordinates: feature.coordinates
+        coordinates
       },
-      properties: {
-        symbol: feature.symbol
-      }
+      properties
     }
   })
 };
@@ -39,4 +39,10 @@ const layout = {
   'icon-size': 1
 };
 
+function handleSymbolClick(event: MapMouseEvent) {
+  if (event.features && event.features.length > 0) {
+    const feature = event.features[0];
+    console.log('Dados do ponto:', feature.properties);
+  }
+}
 </script>
