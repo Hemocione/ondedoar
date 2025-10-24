c8736dd4525180535ab71818a90500843396f2b1
Victor Leonardi <victorleonardireis@poli.ufrj.br>
2025-10-23 23:44:06 -0300

    Update style to match figma

--- components/TypeTag.vue
+++ components/TypeTag.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="inline-flex justify-center items-center px-1 py-0.5 rounded-md text-[0.65rem] font-bold"
+  <div class="inline-flex justify-center items-center px-2 py-0.5 rounded-full text-[0.65rem] font-bold"
     :style="{ backgroundColor: backgroundColor, color: color }">
     {{ name }}
   </div>
@@ -7,18 +7,17 @@
 
 <script setup lang="ts">
 const props = defineProps<{
-  name: string,
   type: string
 }>()
 
 const typeConfig: Record<string, { color: string, backgroundColor: string, name: string }> = {
   askforhelp: { color: '#784000', backgroundColor: '#78400030', name: 'Pedido de Ajuda' },
-  bloodbank: { color: '#B03DD0', backgroundColor: '#B03DD030', name: 'Bancos de Sangue' },
+  bloodbank: { color: '#3DD0BF', backgroundColor: '#3DD0BF30', name: 'Bancos de Sangue' },
   event: { color: '#B03DD0', backgroundColor: '#B03DD030', name: 'Evento' },
   hemocenter: { color: '#3D53D0', backgroundColor: '#3D53D030', name: 'Hemocentro' },
   hospital: { color: '#BB0A08', backgroundColor: '#BB0A0830', name: 'Hospital' }
 }
-
+console.log(props.type)
 const { color, backgroundColor, name } = typeConfig[props.type]
   || {
   color: '#000000',
