03625e584a1a60435f0f84d1a9e89902b5bbeb48
Victor Leonardi <victorleonardireis@poli.ufrj.br>
2025-10-23 23:46:15 -0300

    Adjust padding

--- components/ItemShortInfo.vue
+++ components/ItemShortInfo.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col p-7">
     <span class="title font-bold text-small">Teste</span>
     <span class="subtitle font-normal text-xs text-gray-500">{{ formatSubtitle(distance, address) }}</span>
-    <TypeTag :type="type" class="self-start" />
+    <TypeTag :type="type" class="self-start mt-1" />
   </div>
 </template>
 
