ed01465e9cfead7c20528245915e63df429b1af9
Victor Leonardi dos Reis <victorleonardireis@poli.ufrj.br>
2025-10-25 17:48:32 -0300

    Create loading state

--- composables/states.ts
+++ composables/states.ts
@@ -2,3 +2,6 @@ import { useState } from '#app';
 
 // Armazena a lista de propriedades das features visíveis no mapa
 export const useVisibleFeatures = () => useState<any[]>('visibleFeatures', () => []);
+
+// Armazena o estado de carregamento das features visíveis
+export const useLoadingVisibleFeatures = () => useState<boolean>('loadingVisibleFeatures', () => true);
