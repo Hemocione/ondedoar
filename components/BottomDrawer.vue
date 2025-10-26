45975ec6cb40f8aa818bf1d94d4002dd6593b4b3
Victor Leonardi dos Reis <victorleonardireis@poli.ufrj.br>
2025-10-26 09:29:51 -0300

    Try solution with transition

--- components/BottomDrawer.vue
+++ components/BottomDrawer.vue
@@ -1,19 +1,25 @@
 <template>
   <!-- TODO: REMOVE RING FROM DRAWER UI -->
+  <!-- TODO: THINK OF MOVING THE ELEMENTS INSIDE TEMPLATE CONTENT TO A SLOT -->
   <UDrawer v-model:open="open" :overlay="false" :activeSnapPoint="snapPoint" :dismissible="false" :modal="false"
     :snap-points="[snapPoints.collapsed, snapPoints.partial]" :ui="{ body: 'bg-white', content: 'bg-white' }"
     @update:activeSnapPoint="snapPoint = Number($event)">
     <template #content>
-      <div v-show="snapPoint === snapPoints.collapsed" class="flex flex-col items-center p-4">
-        <USkeleton v-if="loadingVisibleFeatures" class="h-6 w-[120px]" :ui="{ base: 'bg-red-500' }" />
-        <div v-else class="text-hemo-color-text-secondary font-medium">
-          {{ visibleFeaturesCount }} locais visíveis {{ snapPoint }}
+      <Transition name="fade" mode="out-in">
+        <div v-if="snapPoint === snapPoints.collapsed" class="flex flex-col items-center p-4">
+          <USkeleton v-if="loadingVisibleFeatures" class="h-6 w-[120px]" :ui="{ base: 'bg-red-500' }" />
+          <div v-else class="text-hemo-color-text-secondary font-medium">
+            {{ visibleFeaturesCount }} locais visíveis
+          </div>
         </div>
-      </div>
 
-      <div v-show="snapPoint !== snapPoints.collapsed">
-        <UButton label="Para collapsed" class="h-96 m-4" @click="snapPoint = snapPoints.collapsed" />
-      </div>
+        <div v-else>
+          <div v-for="visibleFeature in visibleFeatures" :key="visibleFeature.name">
+            <USkeleton v-if="loadingVisibleFeatures" class="h-6 w-[120px]" :ui="{ base: 'bg-red-500' }" />
+            <ItemShortInfo :distance="'10km'" :address="visibleFeature.address" :type="visibleFeature.type" />
+          </div>
+        </div>
+      </Transition>
     </template>
 
   </UDrawer>
@@ -34,3 +40,15 @@ const visibleFeaturesCount = computed(() => visibleFeatures ? visibleFeatures.va
 const loadingVisibleFeatures = useLoadingVisibleFeatures();
 
 </script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
