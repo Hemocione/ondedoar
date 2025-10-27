18b3a282668ca95920bffff94608efc91d6a83a1
Victor Leonardi dos Reis <victorleonardireis@poli.ufrj.br>
2025-10-27 09:52:57 -0300

    Apply function to search address

--- components/PlaceSearchInput.vue
+++ components/PlaceSearchInput.vue
@@ -3,11 +3,12 @@
   <div class="flex justify-center w-full">
     <UInput v-model="place" variant="soft" size="md" type="text" placeholder="Buscar endereço... "
       icon="i-heroicons-magnifying-glass"
-      :ui="{ base: 'bg-hemo-color-text-primary text-hemo-color-text-secondary rounded-full shadow-lg/5 w-[80vw] text-lg focus:bg-hemo-color-text-primary focus:text-hemo-color-text-secondary hover:bg-hemo-color-text-primary', leadingIcon: 'w-5 h-5', }">
+      :ui="{ base: 'bg-hemo-color-text-primary text-hemo-color-text-secondary rounded-full shadow-lg/5 w-[80vw] text-lg focus:bg-hemo-color-text-primary focus:text-hemo-color-text-secondary hover:bg-hemo-color-text-primary', leadingIcon: 'w-5 h-5', }"
+      @keydown.enter.prevent="searchAddress">
       <template #trailing>
         <UButton variant="link" icon="i-heroicons-adjustments-horizontal"
           :ui="{ base: 'text-hemo-color-text-secondary active:text-hemo-color-primary-light hover:text-hemo-color-primary-action' }"
-          @click="applyFilter" />
+          @click="searchAddress" />
       </template>
     </UInput>
   </div>
@@ -15,13 +16,11 @@
 
 <script lang="ts" setup>
 const place = ref('')
-// TODO: UPDATE FILTER SCHEMA
-const filter = ref('')
+const mapCenter = useMapCenter()
 
-const test = ref(false)
-
-async function applyFilter() {
-  console.log('applyFilter');
-  test.value = !test.value
+async function searchAddress() {
+  const searchedCoordinates = await handleGeocoding(place.value)
+  mapCenter.value = searchedCoordinates
 }
+
 </script>
