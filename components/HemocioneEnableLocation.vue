353782830df0512cdc104e3b1cf783301cea265e
Victor Leonardi dos Reis <victorleonardireis@poli.ufrj.br>
2025-10-29 10:35:42 -0300

    Update hemocioneEnableLocation

--- components/HemocioneEnableLocation.vue
+++ components/HemocioneEnableLocation.vue
@@ -1,15 +1,11 @@
 <template>
-  <div
-    class="enable-location-modal h-screen w-full flex flex-col justify-center z-50 absolute inset-0 bg-white"
-  >
+  <div class="enable-location-modal h-screen w-full flex flex-col justify-center z-50 absolute inset-0 bg-white">
     <div class="image-wrapper flex justify-center items-center m-5 mb-[100px]">
       <div class="w-24 h-24 bg-gray-200 rounded-lg shadow-sm" />
     </div>
     <div class="content-wrapper">
       <div class="content-infos-wrapper w-full px-4 flex flex-col gap-3 mb-[100px]">
-        <h2
-          class="text-center text-lg --hemo-color-text-primary font-semibold mb-2"
-        >
+        <h2 class="text-center text-lg --hemo-color-text-primary font-semibold mb-2">
           Ative a sua localização
         </h2>
 
@@ -19,21 +15,15 @@
         </p>
       </div>
       <div class="buttons-wrapper w-full px-4 flex flex-col gap-3 flex flex-col items-center mt-5 mb-5">
-        <UButton
-          :ui="{
-            base: 'bg-hemo-color-primary text-hemo-color-text-primary hover:bg-hemo-color-primary-action active:bg-hemo-color-secondary active:text-hemo-color-primary-light',
-          }"
-          class="active-local-buttom w-full py-4 text-white hover:bg-red-700 flex justify-center items-center"
-          @click="ativarLocalizacao()"
-        >
+        <UButton :ui="{
+          base: 'bg-hemo-color-primary text-hemo-color-text-primary hover:bg-hemo-color-primary-action active:bg-hemo-color-secondary active:text-hemo-color-primary-light',
+        }" class="active-local-buttom w-full py-4 text-white hover:bg-red-700 flex justify-center items-center"
+          @click="ativarLocalizacao()">
           Permitir
         </UButton>
 
-        <UButton
-          class="not-now-buttom w-full flex justify-center items-center hover:bg-gray-100 rounded-md"
-          color="neutral"
-          @click="closeModal()"
-        >
+        <UButton class="not-now-buttom w-full flex justify-center items-center hover:bg-gray-100 rounded-md"
+          color="neutral" @click="closeModal()">
           Agora não
         </UButton>
       </div>
@@ -41,8 +31,6 @@
   </div>
 </template>
 <script lang="ts" setup>
-import { useLocationPermission } from "~/composables/states";
-
 const locationPermission = useLocationPermission();
 
 function ativarLocalizacao() {
