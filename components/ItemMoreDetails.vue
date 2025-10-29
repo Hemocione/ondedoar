a41c4c80ead95276884d8cc7aa78a7a786678f58
Victor Leonardi dos Reis <victorleonardireis@poli.ufrj.br>
2025-10-29 12:01:21 -0300

    Init new component

---
+++ components/ItemMoreDetails.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="flex flex-col justify-center items-start">
+    <div class="header">
+      <span class="title text-2xl font-semibold">{{ name }}</span>
+      <!-- TODO: FIX FORMAT FOR THIS PAGE -->
+      <span class="subtitle font-normal text-xs text-gray-500">{{ formatSubtitle(distance, address) }}</span>
+      <TypeTag :type="type" class="self-start mt-1" />
+    </div>
+
+    <div class="content ">
+      <div class="address">
+        <img class="w-4 h-4" src="/assets/vectors/RedMapPin.svg" alt="Marcador de mapa vermelho">
+        <span class="text-sm font-normal">{{ address }}</span>
+      </div>
+      <div class="phone" v-if="phone">
+        <img class="w-4 h-4" src="/assets/vectors/RedPhone.svg" alt="Telefone vermelho">
+        <span class="text-sm font-normal">{{ phone }}</span>
+      </div>
+      <div class="link" v-if="link">
+        <img class="w-4 h-4" src="/assets/vectors/RedLink.svg" alt="Globo vermelho com um cursor vermelho">
+        <span class="text-sm font-normal">{{ link }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  active: boolean,
+  name: string,
+  phone?: string,
+  link?: string,
+  type?: string,
+  distance?: string,
+  address?: string
+}>();
+
+</script>
