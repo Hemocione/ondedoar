9550c3478650ba46b1ac07275b7b55045633a08a
Victor Leonardi dos Reis <victorleonardireis@poli.ufrj.br>
2025-11-06 10:28:33 -0300

    Create map Store

---
+++ store/map.ts
@@ -0,0 +1,27 @@
+export const useMapStore = defineStore("map", {
+  // TODO: IMPROVE TYPING
+  state: () => ({
+    pinMarkersFeatures: [] as any[],
+    visibleFeatures: [] as any[],
+    loadingVisibleFeatures: true as boolean,
+    mapCenter: [-43.9345, -19.9167] as number[],
+  }),
+
+  getters: {
+    getMapCenter: (state) => state.mapCenter,
+    getVisibleFeatures: (state) => state.visibleFeatures,
+    isLoadingVisibleFeatures: (state) => state.loadingVisibleFeatures,
+    getPinMarkersFeatures: (state) => state.pinMarkersFeatures,
+  },
+  actions: {
+    setLoadingVisibleFeatures(val: boolean) {
+      this.loadingVisibleFeatures = val;
+    },
+
+    updateVisibleFeatures(features: any[]) {
+      this.visibleFeatures = features;
+    },
+
+
+  }
+})
