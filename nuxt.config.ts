34fc9f3aa812c8d7ccc6c9b631934e83ed0595a6
Victor Leonardi <victorleonardireis@poli.ufrj.br>
2025-11-24 19:46:14 -0300

    Add title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -26,6 +26,21 @@ console.log("Site URL:", siteUrl);
 
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    head: {
+      title: "Onde Doar Sangue - Hemocione",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          name: "description",
+          content: "Encontre a oportunidade de doar sangue mais próximo de você.",
+        },
+        { name: "format-detection", content: "telephone=no" },
+      ],
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    },
+  },
   css: ["~/assets/css/global.css"],
   modules: ["@nuxt/eslint", "nuxt-vercel-analytics", "@nuxt/ui", "@pinia/nuxt"],
   vite: {
