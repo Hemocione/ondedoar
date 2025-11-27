1a2906b81593768ccd0c3b47df90feddf19b820d
Daniel Yoneshige Junior <yoneshige@Daniels-MacBook-Pro.local>
2025-11-27 18:48:36 -0300

    changing navigator query

--- components/HemocioneEnableLocation.vue
+++ components/HemocioneEnableLocation.vue
@@ -1,6 +1,10 @@
 <template>
+<<<<<<< HEAD
   <div
     class="enable-location-modal w-full flex flex-col justify-center z-[100] fixed inset-0 bg-white pt-[env(safe-area-inset-top)] pb-[env(safe-area-inset-bottom)]">
+=======
+  <div class="enable-location-modal h-screen w-full flex flex-col justify-center z-[100] absolute inset-0 bg-white">
+>>>>>>> 37538ae (changing navigator query)
     <div class="image-wrapper flex justify-center items-center m-5 mb-[100px]">
       <img src="/assets/vectors/HemocioneLogo.svg" alt="Logo Gota Hemocione" class="w-48 h-48">
     </div>
@@ -16,21 +20,23 @@
         </p>
       </div>
       <div class="buttons-wrapper w-full px-4 flex flex-col gap-3 flex flex-col items-center mt-5 mb-5">
-        <UButton :ui="{
+        <UButton
+:ui="{
           base: 'bg-hemo-color-primary text-hemo-color-text-primary hover:bg-hemo-color-primary-action active:bg-hemo-color-secondary active:text-hemo-color-primary-light',
         }" class="active-local-buttom w-full py-4 text-white hover:bg-red-700 flex justify-center items-center"
           @click="ativarLocalizacao()">
           Permitir
         </UButton>
 
-        <UButton class="not-now-buttom w-full flex justify-center items-center hover:bg-gray-100 rounded-md"
+        <UButton
+class="not-now-buttom w-full flex justify-center items-center hover:bg-gray-100 rounded-md"
           color="neutral" @click="closeModal()">
           Agora não
         </UButton>
       </div>
     </div>
   </div>
-</template>
+</div></template>
 <script lang="ts" setup>
 import { useUserStore } from '~/store/users';
 
